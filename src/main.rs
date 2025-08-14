4194c0047ea44b45c63a8ae77e09512dd15d3b6c
Brandon AMT <bhernandez@appliedmfg.com>
2025-08-14 15:14:01 -0400

    Added cmd mode initiated by `:`

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use eframe::{egui, NativeOptions, egui::ViewportBuilder};
-use directories::ProjectDirs;
+// use directories::ProjectDirs;
 use rusqlite::{params, Connection};
-use std::fs;
+// use std::fs;
 
-use std::time::Duration;
-use std::thread;
+use std::time::{Instant};
 
 // Toasts
 use egui_toast::{Toast, Toasts, ToastKind, ToastOptions};
-
-use crossbeam_channel::{unbounded, Receiver, Sender};
-
-
 
 // This method creates the data.sqlite file in C:\Users\bhernandez\AppData\Roaming\ExampleOrg...
 // Why don't we just put it next to the executable? Let's do that... 
@@ -24,7 +19,7 @@ use crossbeam_channel::{unbounded, Receiver, Sender};
 // }
 
 fn db_path() -> std::path::PathBuf {
-    std::path::PathBuf::from("data.sqlite")
+    std::path::PathBuf::from("data_test.db")
 }
 
 fn open_db() -> rusqlite::Result<Connection> {
@@ -42,63 +37,22 @@ fn open_db() -> rusqlite::Result<Connection> {
     )?;
     Ok(conn)
 }
-
-// --------------------------- Hotkey wiring ---------------------------
-mod ghk_win {
-    use super::*;
-    use windows_hotkeys::{HotkeyManager, HotkeyManagerImpl};
-    use windows_hotkeys::keys::{ModKey, VKey};
-
-    /// Runs in a background thread: registers key combination and dispatches events
-    pub fn spawn_hotkey_listener(tx: Sender<()>) {
-        thread::spawn(move || {
-            let mut mgr = HotkeyManager::new();
-
-            let res = mgr.register(VKey::N, &[ModKey::Ctrl, ModKey::Shift], {
-                let tx = tx.clone();
-                move || {
-                    let _ = tx.send(());
-                }
-            }).expect("failed to register key combination");
-
-            println!("{:?}", res);
-
-            let res2 = mgr.register(VKey::A, &[ModKey::Ctrl, ModKey::Shift], {
-                let tx = tx.clone();
-                move || {
-                    let _ = tx.send(());
-                }
-            }).expect("failed to register key combination");
-
-            println!("{:?}", res2);
-
-            // Blocks here; processes hotkey callbacks on this thread:
-            mgr.event_loop();
-        });
-    }
-}
-
 // --------------------------- App ---------------------------
 fn main() -> Result<(), eframe::Error> {    
 
     let options = NativeOptions {
         viewport: ViewportBuilder::default()
-            .with_inner_size([300.0, 300.0])    
+            .with_inner_size([300.0, 150.0])    
             .with_resizable(true)
             .with_visible(false),                 // this does nothing
         ..Default::default()
     };
-    
-    // Channel for hotkey events
-    let (tx, rx) = unbounded::<()>();
-
-    ghk_win::spawn_hotkey_listener(tx);
 
     // run_native() runs update() under the hood.
     eframe::run_native(
         "topic v1.0",
         options,
-        Box::new(|_cc| Ok(Box::new(TopicApp::new(rx).expect("db open")))),
+        Box::new(|_cc| Ok(Box::new(TopicApp::new().expect("db open")))),
     )
 }
 
@@ -110,16 +64,22 @@ struct TopicApp {
     conn: Connection,
     topic: String,
     content: String,
-    // delimiter: usize,
     topic_index: usize,
     contents_index: usize,
     toasts: Toasts,
-    close_after: Option<std::time::Instant>,
-    hotkey_rx: Receiver<()>,
+    close_after: Option<Instant>,
+    cmd_mode: bool,
+    cmd: CMD,
+}
+
+enum CMD {
+    Show,
+    Exit,
+    None,
 }
 
 impl TopicApp {
-    fn new(hotkey_rx: Receiver<()>) -> rusqlite::Result<Self> {
+    fn new() -> rusqlite::Result<Self> {
 
         let toasts = Toasts::new()
             .anchor(egui::Align2::RIGHT_BOTTOM, (-12.0, -12.0))
@@ -131,12 +91,12 @@ impl TopicApp {
             conn: open_db()?,
             topic: String::new(),
             content: String::new(),
-            // delimiter: 0,
             topic_index: 0,
             contents_index: 0,
             toasts,
             close_after: None,
-            hotkey_rx,
+            cmd_mode: false,
+            cmd: CMD::None,
         })
     }
     fn save_current(&mut self) {
@@ -204,42 +164,35 @@ impl TopicApp {
 
     }
 
-    fn bring_up_input(&mut self, ctx: &egui::Context) {
-        // Unminimize, bring to front, and focus input
-        use egui::viewport::ViewportCommand as VC;
-        ctx.send_viewport_cmd(VC::Minimized(false));
-        ctx.send_viewport_cmd(VC::Focus);
-        // Request focus to the text field on next frame:
-        self.first_frame = false;
-    }
-
-    fn delimiter(&mut self) {
+    fn search_separator(&mut self) {
         let text_bytes = self.raw_text.as_bytes();
 
-        let mut delimiter: [u8; 3] = [0, 0, 0];
+        let mut separator: [u8; 3] = [0, 0, 0];
         
+        self.topic_index = 0;
+        self.contents_index = 0;
+        self.cmd_mode = false;
+
         for i in 0..text_bytes.len() {
-            
+            // Anytime `:` is the first char we are in cmd mode.
+            if text_bytes[0] == b':' {
+                self.cmd_mode = true;
+                return;
+            }
             // not the most elegant solution...
             if i + 2 < text_bytes.len()  {
-                delimiter[0] = text_bytes[i + 0];
-                delimiter[1] = text_bytes[i + 1];
-                delimiter[2] = text_bytes[i + 2];
+                separator[0] = text_bytes[i];
+                separator[1] = text_bytes[i + 1];
+                separator[2] = text_bytes[i + 2];
             }
 
-            // println!("{:?}", delimiter);
-
-            if delimiter[0] == b',' && delimiter[1] == b',' && delimiter[2] == b' ' {
-                // self.delimiter = i;
+            if separator[0] == b':' && separator[1] == b':' && separator[2] == b':' {
                 self.topic_index = i + 3;
                 self.contents_index = i;
                 return;
             }
-        }
 
-        // self.delimiter = 0;
-        self.topic_index = 0;
-        self.contents_index = 0;
+        }
     }
 
     fn set_topic(&mut self) {
@@ -270,6 +223,28 @@ impl TopicApp {
         self.content = String::new();
     }
 
+    fn get_set_cmd(&mut self) {
+        // Avoid invalid index
+        if self.raw_text.len() > 4 {
+            let cmd = &self.raw_text[1..5]; //Get 4 chars after ":" at [0]
+            let cmd = cmd.to_lowercase();
+            let cmd = cmd.as_str();
+            match cmd {
+                "exit" => {
+                    self.cmd = CMD::Exit;
+                    return;
+                },
+                "show" => {
+                    self.cmd = CMD::Show;
+                    return;
+                },
+                _ => (),
+            }
+        }
+        // If above code did not run
+        self.cmd = CMD::None;
+    }
+
 
 
 
@@ -277,18 +252,12 @@ impl TopicApp {
 
 impl eframe::App for TopicApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        
-        // React to global hotkey
-        while self.hotkey_rx.try_recv().is_ok() {
-            self.bring_up_input(ctx);
-        }
-        
         // Check for key presses
         // esc --> exit
         // enter --> save
         if ctx.input(|i| i.key_pressed(egui::Key::Escape)) || ctx.input(|i| i.viewport().close_requested()) {
-            // ctx.send_viewport_cmd(egui::viewport::ViewportCommand::Close);
-            ctx.send_viewport_cmd(egui::viewport::ViewportCommand::Minimized(true));
+            ctx.send_viewport_cmd(egui::viewport::ViewportCommand::Close);
+            // ctx.send_viewport_cmd(egui::viewport::ViewportCommand::Minimized(true));
             return;
         }
         if ctx.input(|i| i.key_pressed(egui::Key::Enter)) {
@@ -296,8 +265,9 @@ impl eframe::App for TopicApp {
             return;
         }
         
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("content,, topic");
+        egui::CentralPanel::default().show(&ctx, |ui| {
+            // title
+            ui.heading("content:::topic");
 
             // Create a persistent ID for the text field
             let id = ui.make_persistent_id("input");
@@ -308,24 +278,47 @@ impl eframe::App for TopicApp {
                 self.first_frame = true;
             }
 
+            // ~~~Space~~~
+            ui.label("");
+
             // Draw the text input with the given ID
             ui.add(egui::TextEdit::singleline(&mut self.raw_text).id(id));
-
             // ui.text_edit_singleline(&mut self.text);
-            
-            // Search for the delimiter character in input
-            self.delimiter();
 
-            if !self.raw_text.is_empty() && self.topic_index > 0 && self.contents_index > 0 {
+            // ~~~Space~~~
+            ui.label("");
+            
+            // Search for the separator in text input
+            self.search_separator();
+
+            // If the separator is found, topic_index and contents_index will be greater than 0.
+            if self.topic_index > 0 && self.contents_index > 0 {
+                // These setters use the indices internally to separate content from topic.
+                // The texts are trimmed internally.
                 self.set_topic();
                 self.set_content();
                 ui.label(format!("Topic: {}", self.get_topic()));
-                ui.label(format!("Content: {}", self.get_content()));
+                ui.label(format!("{}", self.get_content()));
             }
 
-            // if !self.text.is_empty() {
-            //     ui.label(format!("{}", self.text));
-            // }
+            // println!("Command mode: {}", self.cmd_mode);
+
+            if self.cmd_mode {
+                ui.label(egui::RichText::new("cmd mode").color(egui::Color32::RED));
+                // The following abomination turns text cmd into enum cmd. Sets self.cmd internally.
+                self.get_set_cmd();
+                match self.cmd {
+                    CMD::Show => {
+                        // tbd
+                    },
+                    CMD::Exit => {
+                        ctx.send_viewport_cmd(egui::viewport::ViewportCommand::Close);
+                    },
+                    CMD::None => {
+                        // do nothing
+                    },
+                }
+            }
 
             // Draw toasts each frame (must be last so they overlay UI nicely)
             self.toasts.show(ctx);
@@ -334,11 +327,10 @@ impl eframe::App for TopicApp {
             if let Some(close_time) = self.close_after {
                 if std::time::Instant::now() >= close_time {
                     // ctx.send_viewport_cmd(egui::viewport::ViewportCommand::Close)
-                    ctx.send_viewport_cmd(egui::viewport::ViewportCommand::Minimized(true));
+                    // ctx.send_viewport_cmd(egui::viewport::ViewportCommand::Minimized(true));
                     self.close_after = None;
                 }
             }
-            
         });
     }
 }
